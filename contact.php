999afc76229b44092f8436a3c80ba2f8afd6f354
unknown <kmdubrey@gmail.com>
2021-03-24 14:23:44 -0400

    Contact beginings

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php
+$name =$_POST['name'];
+$phone=$_POST['phone'];
+$email=$_POST['email'];
+$message=$_POST['message'];
+$msg="Name: ".$name."<br>
+Phone: ".$phone."<br>
+Email: ".$email."<br>
+Message: ".$message;
+
+mail("chocolatepbcups@gmail.com","A New Message from Wishin4You",$msg)
+?>
