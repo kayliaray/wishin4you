2a3d2210a4480f8f363b3cceb49d41aed42b8157
unknown <kmdubrey@gmail.com>
2021-03-26 12:24:40 -0400

    Finalizing

--- contact.php
+++ contact.php
@@ -3,11 +3,11 @@ $name =$_POST['name'];
 $phone=$_POST['phone'];
 $email=$_POST['email'];
 $message=$_POST['message'];
-$msg="Name: $name <br>
-Phone: $phone<br>
-Email: $email<br>
+$msg="Name: $name 
+Phone: $phone
+Email: $email
 Message: $message";
 
 mail("contact@wishin4you.com","A New Message from Wishin4You",$msg);
-header("Location:'contact_confirm.html'");
+header("Location:contact_confirm.html");
 ?>
