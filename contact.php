f263ec0b997567d7a4a7adf957b50917084700a8
unknown <kmdubrey@gmail.com>
2021-03-26 09:53:21 -0400

    what is a string literal

--- contact.php
+++ contact.php
@@ -3,10 +3,10 @@ $name =$_POST['name'];
 $phone=$_POST['phone'];
 $email=$_POST['email'];
 $message=$_POST['message'];
-$msg="Name: ".$name."<br>
-Phone: ".$phone."<br>
-Email: ".$email."<br>
-Message: ".$message;
+$msg="Name: $name <br>
+Phone: $phone<br>
+Email: $email<br>
+Message: $message";
 
 mail("contact@wishin4you.com","A New Message from Wishin4You",$msg);
 header("Location:'contact_confirm.html'");
